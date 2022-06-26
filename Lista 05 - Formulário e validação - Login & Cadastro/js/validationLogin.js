652688f09274c015184617aa32d25edf7843ce95
giovanagvi <giovana.gvi02@gmail.com>
2022-06-26 01:44:17 -0300

    update validations

--- Lista 05 - Formulário e validação - Login & Cadastro/js/validationLogin.js
+++ Lista 05 - Formulário e validação - Login & Cadastro/js/validationLogin.js
@@ -1,9 +1,23 @@
-window.onload= function(){
+window.onload = function(){
+    //TUDO OK
+    const myModal = new bootstrap.Modal('#MyModal', {
+        keyboard: false
+    })
+    myModal.show()
 
     const button = document.getElementById("btn-login")
-    const email = document.getElementById("")
+    const email = document.getElementById("email")
+    const password = document.getElementById("password")
 
-    button.addEventListener("click", function(){
 
+    document.addEventListener('keyup', function(){
+        console.log(email.value)
+        console.log(password.value)
+
+        if(email.value.length > 0 && password.value.length > 0){
+            button.disabled = false
+        }else{
+            button.disabled = true
+        }
     })
 }
--- Lista 05 - Formulário e validação - Login & Cadastro/js/validationSignup.js
+++ Lista 05 - Formulário e validação - Login & Cadastro/js/validationSignup.js
@@ -1,71 +1,7 @@
-window.onload= function(){
+window.onload = function(){
 
-    //ELEMENTOS
-const button = document.getElementById("btn-subscribe")
-const name = document.getElementById("name")
-const email = document.getElementById("email")
-const password = document.getElementById("password")
-const confirm = document.getElementById("passwordConfirm")
+// VALIDAR DATAS E FINALIZAR STYLE
 
-const daySelect = document.getElementById("selectDay")
-const monthSelect = document.getElementById("selectMonth")
-const yearSelect = document.getElementById("selectYear")
-    
-    //ATRIBUTOS
-const months = ['Janeiro', 'Fevereiro', 'Março', 'Abril', 'Maio', 'Junho', 'Julho', 'Agosto',
-'Setembro', 'Outubro', 'Novembro', 'Dezembro']
+// CHECAR ENUNCIADO DA QUESTÃO PARA VER SE ESTÁ TUDO OK
 
-    //EVENTOS
-
-    //FUNÇÕES
-function popularMeses(){
-    for(let i=0; i<months.length; i++){
-        const option = document.createElement('option')
-        option.textContent = months[i]
-        monthSelect.appendChild(option)
-    }
-}
-function popularDias(mes){
-    //deleta todos os filhos do select day
-    //se eles existirem
-    while(daySelect.firstChild){
-        daySelect.removeChild(daySelect.firstChild)
-    }
-    //guarda o número de dias do mês
-    let dia
-
-    if(mes==='Janeiro' || mes==='Março' || mes==='Julho' 
-    || mes==='Agosto' || mes==='Outubro'|| mes==='Dezembro'){
-        dia = 31
-    }else if (mes==='Abril' || mes==='Junho' || mes==='Setembro' || mes==='Novembro'){
-        dia = 30
-    }else{
-        
-    }
-    //Inserir o dia correto no select de dia
-    for(let i=1; i<=dia; i++){
-        const option = document.createElement('option')
-        option.textContent = i
-        daySelect.appendChild(option)
-    }
-}
-function popularAnos(){
-    //pegar o ano atual 
-    let ano = new Date().getFullYear()
-    
-    //deixar alguns anos passados como options
-    for(let i=0; i < 101; ano++){
-        const option = document.createElement('option')
-        option.textContent = ano - i
-        yearSelect.appendChild(option)
-    }
-}
-
-    //EVENTOS
-    //button.addEventListener("click", function(){
-
-    //})
-popularMeses()
-popularDias(monthSelect.value)
-popularAnos()
 }
